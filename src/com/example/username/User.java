cb9489ebc00cf70031f2f517cef296cac29454cd
t0in4 <t0in4@yahoo.com>
2023-01-11 21:31:07 +0900

    Initial commit

---
+++ src/com/example/username/User.java
@@ -0,0 +1,14 @@
+package com.example.username;
+
+public class User {
+    int x;
+    int y;
+    public User(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public int sum() {
+        return this.x + this.y;
+    }
+
+}
---
+++ src/org/example/username/User2.java
@@ -0,0 +1,10 @@
+package org.example.username;
+import com.example.username.User;
+import java.util.Scanner;
+public class User2 {
+
+    public static void main(String[] args) {
+        User userCom = new User(2, 3);
+        System.out.println(userCom.sum());
+    }
+}
